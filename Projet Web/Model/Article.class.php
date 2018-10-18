428b53a826075d1cbde354090a318d301323c9da
HILKENS Bram <hilkensb@hostname.iut2.upmf-grenoble.fr>
2018-10-18 10:38:09 +0200

    fichier créant la base de données , rajout classe  model

---
+++ Projet Web/Model/Article.class.php
@@ -0,0 +1,12 @@
+<?php
+    class Article{
+      private $réf;
+      private $nom;
+      private $caract;
+      private $prix;
+      private $photo
+      private $catégorie;
+      private $marque;
+      private $desc;
+    }
+?>
