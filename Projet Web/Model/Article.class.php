386255d64e3157437c7aca872a53bbf1fbe38b5e
Téo <teo.poujois@wanadoo.fr>
2018-10-18 11:21:49 +0200

    Modif class.php

--- Projet Web/Model/Article.class.php
+++ Projet Web/Model/Article.class.php
@@ -1,12 +1,28 @@
 <?php
-    class Article{
-      private $réf;
+   class Article{
+      private $ref;
       private $nom;
       private $caract;
       private $prix;
-      private $photo
-      private $catégorie;
+      private $photo;
+      private $categorie;
       private $marque;
-      private $desc;
+		private $desc;
+		private $date;
     }
+
+	function __construct($ref, $nom, $caract, $prix, $photo=NULL,$categorie,$marque,$desc=NULL){
+		$this->ref = $ref;
+		$this->nom = $nom;
+		$this->caract = $caract;
+		$this->prix = $prix;
+		$this->photo = $photo;
+		$this->categorie = $categorie;
+		$this->marque = $marque;
+		$this->desc = $desc;
+   }
+
+   function __get(string $property){
+      return $this->$property;
+   }
 ?>
