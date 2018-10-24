b539e135e622bc6d17864bda5b6e4a8b60c966a7
Zunkusem <jeremi04@hotmail.fr>
2018-10-24 16:46:18 +0200

    Modif pageArticle + css test

--- Projet Web/View/articles.view.php
+++ Projet Web/View/articles.view.php
@@ -1,51 +1,68 @@
-<html>
-<head>
-  <title>Bricomachin</title>
-  <meta charset="UTF-8"/>
-  <meta http-equiv="content-type" content="text/html;" />
-  <meta name="author" content="Jean-Pierre Chevallet" />
-  <link rel="stylesheet" type="text/css" href="../view/design/style.css"
-</head>
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+	<head>
+		<meta charset="utf-8">
+		<title>RatKistan Produit</title>
+		<link rel="stylesheet" href="design/styleProtoArticlePage.css">
+	</head>
+	<body>
+		<header>
+			<img id="Headerimg1" src="Img/Logo.jpg"/ width="100" height="100">
+		  	<h1>"IL Est Increvable"</h1>
+		</header>
+		<nav>
+		  <ul id="Navhaut">
+			 <li><a href="#">Nouveau Articles</a></li>
+			 <li><a href="#">Lorem</a></li>
+			 <li><a href="#">Lorem ipsum dolor sit amet.</a></li>
+		  </ul>
+		</nav>
 
-<body>
-  <header>
-    <h1>Bricomachin, le bricolage malin </h1>
-    <p><img src="../view/design/pub.png"/></p>
-  </header>
-  <?php
+		<div id=mainArticle>
+			<div id = Description>
+				<div>
+		        <img src="Img/LogoProto0000.jpg"/ width= 300 height="300">
+	      	</div>
+				<div class="">
+					<h1>Carte Graphique GTXRagondIncrevable[P]</h1>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+					<h1 id= prix>649.99€</h1>
+				</div>
+			</div>
+			<div id = FicheTeck>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+			</div>
+		</div>
 
+		<div id = Article_Recomander>
+			<h3>Article recommander</h3>
+			<div class="">
+				<nav>
+				  <ul id="NavArticle_Recomander">
+						<li> <img src="Img/LogoProto0000.jpg"/ width= "130" height="130">
+						<li> <img src="Img/LogoProto0000.jpg"/ width= "130" height="130">
+						<li> <img src="Img/LogoProto0000.jpg"/ width= "130" height="130">
+						<li> <img src="Img/LogoProto0000.jpg"/ width= "130" height="130">
+						<li> <img src="Img/LogoProto0000.jpg"/ width= "130" height="130">
+				  </ul>
+				</nav>
+			</div>
+      </div>
 
-  ///////////////////////////////////////////////////////
-  //   A COMPLETER
-  ///////////////////////////////////////////////
-  // Si une categorie est définie
-	if (empty($categorie)){
-		$categorie = $articles[0]->categorie;
-    }
-	$dao->getCat($categorie)->getpath();
-  ?>
+		<footer>
+			<nav>
+			  <ul id="NavBas">
+				 <li><a href="#">1</a></li>
+				 <li><a href="#">2</a></li>
+				 <li><a href="#">3</a></li>
+				 <li><a href="#">4</a></li>
+				 <li><a href="#">5</a></li>
+				 <li><a href="#">6</a></li>
+			  </ul>
+			</nav>
+		</footer>
 
-  <nav>
-    <!-- Bouton de retour au début de la liste -->
-    <a href="?"><img src="../view/design/home.png"/></a>
-    <!-- Bouton de retour à la page précédente -->
-    <a href="?ref=<?= end($prev)->ref .'&categorie='.$categorie?>">&lt;  </a>
-    <!-- Bouton pour passer à la page suivante -->
-    <a href="?ref=<?= $nextRef.'&categorie='.$categorie?>">></a>
-  </nav>
-
-  <?php
-  ///////////////////////////////////////////////////////
-  //  A COMPLETER
-  ///////////////////////////////////////////////
-
-  // Affichage des articles
-  ?>
-  <?php for($i =0;$i<12;$i++){ ?>
-    <article> <h2> <?= $articles[$i]->libelle ?></h2> <img src="http://www-info.iut2.upmf-grenoble.fr/intranet/enseignements/ProgWeb/data/bricomachin/img/<?= $articles[$i]->image ?>" /><?=$articles[$i]->prix ?> </article>
-  <?php }  ?>
-  <footer>
-  </p>Site fictif, issus de données réelles du Web</p>
-</footer>
-</body>
+	</body>
 </html>
