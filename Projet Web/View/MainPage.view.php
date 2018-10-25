858bb6c406f4465ed7a6bb3d7856ff81dde65dad
Téo <teo.poujois@wanadoo.fr>
2018-10-25 11:36:00 +0200

    test num page

--- Projet Web/View/MainPage.view.php
+++ Projet Web/View/MainPage.view.php
@@ -51,7 +51,7 @@
 			 <ul id="NavBas">
 
 			   <li><a href="?refartc=<?= $previousPage ?>"><?php $pageActuelle=$pageActuelle-1; ?><</a></li>
-			   <li><?= $pageActuelle=(int)($articleDeDepart/$nbArticlePage)  ?></li>
+			   <li><?= $pageActuelle=(int)((($articleDeDepart+($nbArticlePage/2))+$nbArticlePage)/$nbArticlePage)  ?></li>
 			   <li><a href="?refartc=<?= $nextPage ?>"><?php $pageActuelle=$pageActuelle+1; ?>></a></li>
 			 </ul>
 		  </nav>
