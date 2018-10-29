57a57025e895ef3175856a35348dcd580ec9e622
Hilkensb <unconfigured@null.spigotmc.org>
2018-10-29 14:23:47 +0100

    sdfsd

--- Projet Web/Model/DAO.class.php
+++ Projet Web/Model/DAO.class.php
@@ -27,7 +27,6 @@
 			$req = "SELECT * FROM article WHERE ref=$ref";
 			$res = $this->db->query($req);
 			$resul = $res->fetchall(PDO::FETCH_CLASS,'article');
-      var_dump($resul);
 			return $resul[0];
 		}
 
