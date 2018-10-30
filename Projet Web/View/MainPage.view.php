9138034a5301b0cf5fb171a3396b9081db3b086e
Zunkusem <jeremi04@hotmail.fr>
2018-10-30 18:17:28 +0100

    micro modif

--- Projet Web/View/MainPage.view.php
+++ Projet Web/View/MainPage.view.php
@@ -66,7 +66,7 @@
 		  <nav>
 			 <ul id="NavBas">
 				 <?php echo $pageActuelle; ?>
-			   <li><a href="?refartc=<?= $previousPage ?>&refcat=<?= $refcategorie?>&dispo=<?= $dispo ?>"><?php $pageActuelle-- ?><</a></li>
+			   <li><a href="?refartc=<?= $previousPage ?>&refcat=<?= $refcategorie?>&dispo=<?= $dispo ?>"><?php $pageActuelle=$pageActuelle-1 ?><</a></li>
 				<li><?=  $pageActuelle;// $pageActuelle= (int)(($_GET(['refartc'])+($nbArticlePage/2))/$nbArticlePage) ?></li>
 			   <li><a href="?refartc=<?= $nextPage ?>&refcat=<?= $refcategorie?>&dispo=<?= $dispo ?>"><?php $pageActuelle++ ?>></a></li>
 			 </ul>
